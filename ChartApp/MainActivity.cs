ee0080c85363280bd5a629ac30ae16607a2c3601
Rikysamuel <rikysamuel@xtremax.com>
2017-10-13 12:48:27 +0700

    chart app sample

---
+++ ChartApp/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+using ChartApp.Fragments;
+using Samples.Business.DataModel;
+
+namespace ChartApp
+{
+    [Activity(Label = "ChartApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : FragmentActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+            var viewPager = FindViewById<ViewPager>(Resource.Id.viewpager);
+
+            var chartModels = new List<ChartModel>
+            {
+                new ChartModel() {model = "Plot"},
+                new ChartModel() {model = "Bar"},
+                new ChartModel() {model = "Area"}
+            };
+            var adapter = new ChartPagerAdapter(SupportFragmentManager, chartModels);
+
+            viewPager.Adapter = adapter;
+        }
+    }
+}
+
