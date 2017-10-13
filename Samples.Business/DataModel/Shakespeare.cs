6ca6789df7f262711ba5bcc744cc27f172304d0a
Rikysamuel <rikysamuel@xtremax.com>
2017-10-13 14:56:26 +0700

    fragment app sample

---
+++ Samples.Business/DataModel/Shakespeare.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Samples.Business.DataModel
+{
+    public class Shakespeare
+    {
+        public static List<string> Titles = new List<string>()
+        {
+            "Introduction",
+            "Early Life",
+            "London and theatrical career"
+        };
+
+        public static List<string> Dialogue = new List<string>()
+        {
+            "William Shakespeare (26 April 1564 (baptised) – 23 April 1616)[nb 1] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's pre-eminent dramatist.[2] He is often called England's national poet, and the \"Bard of Avon\".[3][nb 2] His extant works, including collaborations, consist of approximately 38 plays,[nb 3] 154 sonnets, two long narrative poems, and a few other verses, some of uncertain authorship. His plays have been translated into every major living language and are performed more often than those of any other playwright.[4]",
+            "William Shakespeare was the son of John Shakespeare, an alderman and a successful glover originally from Snitterfield, and Mary Arden, the daughter of an affluent landowning farmer.[8] He was born in Stratford-upon-Avon and baptised there on 26 April 1564. His actual date of birth remains unknown, but is traditionally observed on 23 April, Saint George's Day.[9] This date, which can be traced back to an 18th-century scholar's mistake, has proved appealing to biographers because Shakespeare died on 23 April 1616.[10] He was the third child of eight and the eldest surviving son.[11]",
+            "It is not known definitively when Shakespeare began writing, but contemporary allusions and records of performances show that several of his plays were on the London stage by 1592.[28] By then, he was sufficiently known in London to be attacked in print by the playwright Robert Greene in his Groats-Worth of Wit:"
+        };
+    }
+}
